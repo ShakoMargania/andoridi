1215162b07548df72dd5b0ed41dff12b8723f4fd
shako <shalva.margania.1@btu.edu.ge>
2021-12-13 16:50:28 +0400

    davalebai

---
+++ app/src/main/java/com/example/menuapp/HomeFragment.kt
@@ -0,0 +1,6 @@
+package com.example.menuapp
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment: Fragment(R.layout.home_fragment) {
+}
---
+++ app/src/main/java/com/example/menuapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.menuapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNav = findViewById(R.id.navBar)
+
+        val controller = findNavController(R.id.navigation)
+        val appBarConfig = AppBarConfiguration(setOf(R.id.homeFragment, R.id.questionsFragment3, R.id.answersFragment3))
+
+        setupActionBarWithNavController(controller, appBarConfig)
+        bottomNav.setupWithNavController(controller)
+
+    }
+}
